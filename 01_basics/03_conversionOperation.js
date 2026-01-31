6ef1179e0b1c9acb95b207d129144b93d9e8869c
sri-06-124 <srikantmisra2006@gmail.com>
2026-01-31 06:00:20 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+// let scope = 23;
+// console.log(typeof scope)
+
+// let score = "23"
+// console.log(typeof score)
+
+// let value = "23ABC"
+// console.log(typeof value)
+// let valueInNumber11 = Number(value)
+// console.log(valueInNumber11)
+
+// let valueInNumber = Number(value)
+// console.log(valueInNumber)
+
+// let valueInNumber2 = Number.score
+// console.log(typeof valueInNumber2)//undefined
+
+// let temp = null
+// let valueInNumber22 = Number(temp)
+// console.log(valueInNumber22)//0
+
+// let tap = true
+// console.log(typeof tap)
+// let valInNo = Number(tap)
+// console.log(valInNo)
+
+// let no = 1
+// let noInBoolean = Boolean(no)
+// console.log(noInBoolean)
+//1 => true  0=>false
+//" "=> false
+//"Srikant" => true
+
+let num = 33
+let valueString = String(33)
+console.log(valueString) //33
+console.log(typeof valueString) //string
