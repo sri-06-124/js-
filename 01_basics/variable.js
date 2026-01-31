36352c5942f78c2bff2786b8a1bb9c11155f4d8f
sri-06-124 <srikantmisra2006@gmail.com>
2026-01-31 06:02:56 +0000

    variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,10 @@
+const accountId = 124421;
+let usernname = "srikant"
+var accPassword = "124"
+accountCity = "Rayagada"
+let accountState;
+
+/*
+    prefer not to use var, because of issues in block scope and function scope
+*/
+console.table([accountId, usernname, accPassword, accountCity, accountState])
