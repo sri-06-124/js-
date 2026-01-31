c859c7d59de4269153285fb551bc93db6d8dacbe
sri-06-124 <srikantmisra2006@gmail.com>
2026-01-31 04:33:42 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as newer verson
+
+//alert(3 + 3)  //her it will show error bcz her we are using node.js not browser
+
+console.log(3 + 3)
+
+let name = "srikant"
+let age = 18
+let ispositive = false
+let state;
+
+//number => 2 to the power 53
+//bigint
+//string => " "
+//boolean => true/false
+//null => standalone value not undefined or 0
+//undefined => value is not assigned yet
+//symbol => use when we need unique
+
+//object
+
+console.log(typeof "srikant")
+console.log(typeof age)
+console.log(typeof null)//object
+console.log(typeof undefined)
+//output
+/*
+string
+number
+object
+undefined
+*/
