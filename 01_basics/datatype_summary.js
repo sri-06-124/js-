55e6a9d3eb3aecca96583934b7a794e32bf0dbcb
sri-06-124 <srikantmisra2006@gmail.com>
2026-02-03 17:00:53 +0000

    datatype summary

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -33,3 +33,28 @@ const myFunction = function(){
     console.log("Hello World...")
 }
 console.log(typeof myFunction)//function
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//Stack(primitive) and heap memory(non primitive)
+
+let myName = "Srikant";
+let myNewName = myName;//a copy of the value is created
+console.log(myNewName)//Srikant
+myNewName = "Parmpara" ;
+console.log(myName)//srikant
+console.log(myNewName)//Parmpara
+
+let userOne = {
+    name : "rahul",          //here the name and age will store in the heap but 
+    age : 23                      // the userOne is stored in the stack memory 
+                                     
+}
+                                     
+let userTwo = userOne;      /* here the userTwo will directly indicate/reference to the 
+                             heap memory where the name and age is stored*/
+                                                                                           
+console.log(userOne.name)//rahul
+userTwo.name = "ArghyaRupa";
+console.log(userOne.name)//ArghyaRupa
+console.log(userTwo.name)//ArghyaRupa
